bca4ec4f382c986b0604e8edadf2f5f85bf8aec7
Hyejin Im <hyejinim17@gmail.com>
2020-10-23 14:39:49 -0400

    Update button label

--- setup.js
+++ setup.js
@@ -279,13 +279,13 @@ function switchMode() {
 
     codeBtn = createButton('<i class="fa fa-eye"></i> Code');
     codeBtn.id('codeBtn');
-    codeBtn.position(windowW - 100, 79);
+    codeBtn.position(windowW - 100, 139);
     codeBtn.mousePressed(showCode);
 
     if (mobile) {
-      switchBtn = createButton('sync-alt');
+      switchBtn = createButton('<i class="fa fa-sync-alt"></i>');
       switchBtn.id('switchBtn');
-      switchBtn.position(windowW - 100, 49);
+      switchBtn.position(windowW - 100, 79);
       switchBtn.mousePressed(switchCamera);
     }
 
